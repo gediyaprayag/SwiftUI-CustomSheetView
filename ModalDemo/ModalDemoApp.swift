7df86f5ef7e9a809158fb640adde2211be8341b0
Prayag Gediya <prayaggediya60@gmail.com>
2021-05-09 08:57:52 +0530

    Initial Commit

---
+++ ModalDemo/ModalDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModalDemoApp.swift
+//  ModalDemo
+//
+//  Created by mac on 09/05/21.
+//
+
+import SwiftUI
+
+@main
+struct ModalDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
